64b7f1406d0d49df9f82c8342e86a3677d08c380
Ari <ariel.reis@nubank.com.br>
2022-10-22 12:44:39 -0300

    add policy to ecs task

--- ecs_task.tf
+++ ecs_task.tf
@@ -1,9 +1,10 @@
 resource "aws_ecs_task_definition" "cicd_project_service_task" {
-   family                   = "cicd-service-task"
+  family                   = "cicd-service-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = 1024
   memory                   = 2048
+  execution_role_arn       = aws_iam_role.ecs_task_role.arn
   container_definitions    = <<TASK_DEFINITION
 [
   {
