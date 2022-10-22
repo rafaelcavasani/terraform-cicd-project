a786c085b0909b1a666c71ce111351b31e321484
Rafael Cavasani <rafael.cavasani@hotmail.com>
2022-10-22 10:17:28 -0300

    Remove bucket

--- aws-s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "terraform-cicd-project-example-s3"
-}
-
-resource "aws_s3_bucket_acl" "example_bucket_acl" {
-  bucket = aws_s3_bucket.example_bucket.id
-  acl    = "private"
-}
