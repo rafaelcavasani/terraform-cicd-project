80de9784f105d38b3914395acb44397b5b7095e3
Ari <ariel.reis@nubank.com.br>
2022-10-22 12:23:17 -0300

    rename cluster to task definition

--- ecs_service.tf
+++ ecs_service.tf
@@ -1,6 +1,6 @@
 resource "aws_ecs_service" "cicd_project_service" {
     name = "cicd-project-service"
     cluster = aws_ecs_cluster.cicd_project_cluster.id
-    task_definition = aws_ecs_cluster.cicd_project_service_task.arn
+    task_definition = aws_ecs_task_definition.cicd_project_service_task.arn
     desired_count = 1
 }
