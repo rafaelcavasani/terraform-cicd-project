618501cc5f942ef7d41699e83a5b3b9a0bd1c47e
Ari <ariel.reis@nubank.com.br>
2022-10-22 12:51:58 -0300

    change memory and cpu allocation

--- ecs_task.tf
+++ ecs_task.tf
@@ -2,16 +2,16 @@ resource "aws_ecs_task_definition" "cicd_project_service_task" {
   family                   = "cicd-service-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = 1024
-  memory                   = 2048
+  cpu                      = 128
+  memory                   = 512
   execution_role_arn       = aws_iam_role.ecs_task_role.arn
   container_definitions    = <<TASK_DEFINITION
 [
   {
     "name": "cicd-service",
     "image": "625866090828.dkr.ecr.us-east-1.amazonaws.com/terraform-cicd-project-app:latest",
-    "cpu": 1024,
-    "memory": 2048,
+    "cpu": 128,
+    "memory": 512,
     "essential": true
   }
 ]
