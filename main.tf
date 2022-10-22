c401f8b9bd24de85453684150144a9b49bab2cd4
Rafael Cavasani <rafael.cavasani@hotmail.com>
2022-10-22 08:41:13 -0300

    Add main file

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-cicd-project"
+    key = "terraform.tfstate"
+    encrypt = true
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform"
+}
