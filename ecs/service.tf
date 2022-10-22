ee6a3280208dc5df5aee2b38d8c8e157db914116
Ari <ariel.reis@nubank.com.br>
2022-10-22 12:20:25 -0300

    rename ecs definitions

--- ecs/service.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_ecs_service" "cicd_project_service" {
-    name = "cicd-project-service"
-    cluster = aws_ecs_cluster.cicd_project_cluster.id
-    task_definition = aws_ecs_cluster.cicd_project_service_task.arn
-    desired_count = 1
-}
