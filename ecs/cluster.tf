03522b310bd5d7b4f5d5253b9ed87b4a33dce310
Ari <ariel.reis@nubank.com.br>
2022-10-22 12:08:49 -0300

    add ecs cluster

---
+++ ecs/cluster.tf
@@ -0,0 +1,21 @@
+resource "aws_ecs_cluster" "cicd_project_cluster" {
+  name = "cicd-project-cluster"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+}
+
+
+resource "aws_ecs_cluster_capacity_providers" "cicd_project_cluster_capacity_provider" {
+  cluster_name = aws_ecs_cluster.cicd_project_cluster.name
+
+  capacity_providers = ["FARGATE_SPOT"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE_SPOT"
+  }
+}
