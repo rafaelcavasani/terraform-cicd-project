6421b8b18d58aa0845a9348f6bb9233a5e11c75e
Rafael Cavasani <rafael.cavasani@hotmail.com>
2022-10-22 10:15:23 -0300

    Add bucket acl

--- aws-s3.tf
+++ aws-s3.tf
@@ -1,4 +1,8 @@
-resource "aws_s3_bucket_acl" "bucket_example" {
+resource "aws_s3_bucket" "example_bucket" {
   bucket = "terraform-cicd-project-example-s3"
-  region = "us-east-1"
+}
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.example_bucket.id
+  acl    = "private"
 }
