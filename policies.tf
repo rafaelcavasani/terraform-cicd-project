0e0face4ebfecb89db7f97b29ef9c1de238afb5b
Ari <ariel.reis@nubank.com.br>
2022-10-22 12:46:49 -0300

    add policy to ecs task

--- policies.tf
+++ policies.tf
@@ -1,13 +1,6 @@
 resource "aws_iam_role" "ecs_task_role" {
   name                = "cicd-task-role"
-  assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json
-  managed_policy_arns = [aws_iam_policy.ecs_task_role_policy.arn]
-}
-
-resource "aws_iam_policy" "ecs_task_role_policy" {
-  name = "policy-618033"
-
-  policy = jsonencode({
+  assume_role_policy  = jsonencode({
   "Version": "2012-10-17",
   "Statement": [
         {
