f9ac9683bc2c8383d96de67a46a1414dd3dc500c
Rafael Cavasani <rafael.cavasani@hotmail.com>
2022-10-24 23:11:29 -0300

    Change task definition role

--- ecs_task.tf
+++ ecs_task.tf
@@ -4,17 +4,17 @@ resource "aws_ecs_task_definition" "cicd_project_service_task" {
   network_mode             = "awsvpc"
   cpu                      = 256
   memory                   = 512
-  execution_role_arn       = aws_iam_role.ecs_task_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   container_definitions    = <<TASK_DEFINITION
-[
-  {
-    "name": "cicd-service",
-    "image": "625866090828.dkr.ecr.us-east-1.amazonaws.com/terraform-cicd-project-app:latest",
-    "cpu": 128,
-    "memory": 512,
-    "essential": true
-  }
-]
-TASK_DEFINITION
-
+    [
+      {
+        "name": "cicd-service",
+        "image": "625866090828.dkr.ecr.us-east-1.amazonaws.com/terraform-cicd-project-app:latest",
+        "cpu": 128,
+        "memory": 512,
+        "essential": true
+      }
+    ]
+  TASK_DEFINITION
 }
--- policies.tf
+++ policies.tf
@@ -1,40 +1,43 @@
-resource "aws_iam_role" "ecs_task_role" {
-  name = "cicd-task-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-  managed_policy_arns = [aws_iam_policy.ecs_task_role_policy.arn]
-}
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "role-name"
 
-resource "aws_iam_policy" "ecs_task_role_policy" {
-  name = "policy-618033"
-
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+  assume_role_policy = <<EOF
+    {
+    "Version": "2012-10-17",
+    "Statement": [
       {
-        "Effect" : "Allow",
-        "Action" : [
-          "ecr:GetAuthorizationToken",
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:BatchGetImage",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "sts:AssumeRole"
-        ],
-        "Resource" : "*"
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ecs-tasks.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
       }
     ]
-  })
+    }
+  EOF
+}
+resource "aws_iam_role" "ecs_task_role" {
+  name = "role-name-task"
+
+  assume_role_policy = <<EOF
+    {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "ecs-tasks.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+    }
+  EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
