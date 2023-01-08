3734e7d9b7bde662337b494dafd766afa75753d8
Rafael Cavasani <rafael.cavasani@hotmail.com>
2023-01-08 16:18:09 -0300

    format

--- subnet.tf
+++ subnet.tf
@@ -1,11 +1,11 @@
 resource "aws_subnet" "public_1" {
-    vpc_id                  = aws_vpc.main.id
-    cidr_block              = "192.168.0.0/18"
-    availability_zone       = "us-east-1a"
-    map_public_ip_on_launch = true
-    tags = {
-      "name"                      = "public-us-east-1a"
-      "kubernetes.io/cluster/eks" = "shared"
-      "kubernetes.io/role/elb"    = 1
-    }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "192.168.0.0/18"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    "name"                      = "public-us-east-1a"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+  }
 }
