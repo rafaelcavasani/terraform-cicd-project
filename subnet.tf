8de26ea2aac389ef5ff8e2368265b9a7262d87b4
Rafael Cavasani <rafael.cavasani@hotmail.com>
2023-01-08 14:26:40 -0300

    fix vpc id

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "public_1" {
-    vpc_id                  = aws_internet_gateway.main.id
+    vpc_id                  = aws_vpc.main.id
     cidr_block              = "192.168.0.0/18"
     availability_zone       = "us-east-1a"
     map_public_ip_on_launch = true
@@ -11,7 +11,7 @@ resource "aws_subnet" "public_1" {
 }
 
 resource "aws_subnet" "public_2" {
-    vpc_id                  = aws_internet_gateway.main.id
+    vpc_id                  = aws_vpc.main.id
     cidr_block              = "192.168.64.0/18"
     availability_zone       = "us-east-1b"
     map_public_ip_on_launch = true
@@ -23,7 +23,7 @@ resource "aws_subnet" "public_2" {
 }
 
 resource "aws_subnet" "private_1" {
-    vpc_id                  = aws_internet_gateway.main.id
+    vpc_id                  = aws_vpc.main.id
     cidr_block              = "192.168.128.0/18"
     availability_zone       = "us-east-1a"
     map_public_ip_on_launch = true
@@ -35,7 +35,7 @@ resource "aws_subnet" "private_1" {
 }
 
 resource "aws_subnet" "private_2" {
-    vpc_id                  = aws_internet_gateway.main.id
+    vpc_id                  = aws_vpc.main.id
     cidr_block              = "192.168.192.0/18"
     availability_zone       = "us-east-1b"
     map_public_ip_on_launch = true
